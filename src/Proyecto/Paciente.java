c353456f2454f979a1b447b2edbd3dec33a673b1
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 20:40:12 -0300

    Implementar constructor de Paciente con inicializacion de listas e historial

--- src/Proyecto/Paciente.java
+++ src/Proyecto/Paciente.java
@@ -1,5 +1,6 @@
 package Proyecto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Paciente {
@@ -8,4 +9,14 @@ public class Paciente {
     private List<Medicamento> listaMedicamentos;
     private List<Recordatorio> listaRecordatorios;
     private Historial historial;
+
+    //constructor
+    public Paciente(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.listaMedicamentos = new ArrayList<>();
+        this.listaRecordatorios = new ArrayList<>();
+        this.historial = new Historial();
+    }
+
 }
