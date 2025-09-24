c78c967cd551546f22b409c23b57209e3b66cab9
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 20:38:09 -0300

    Agregar atributos principales para gestion completa de paciente

--- src/Proyecto/Paciente.java
+++ src/Proyecto/Paciente.java
@@ -1,4 +1,11 @@
 package Proyecto;
 
+import java.util.List;
+
 public class Paciente {
+    private String nombre;
+    private int edad;
+    private List<Medicamento> listaMedicamentos;
+    private List<Recordatorio> listaRecordatorios;
+    private Historial historial;
 }
