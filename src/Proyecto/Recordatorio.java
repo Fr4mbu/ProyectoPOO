da07293cacc51d76ee2b7ccf6cba9f37c34319b4
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 18:29:00 -0300

    Agregar atributos para gestion de recordatorios con fecha y medicamento

--- src/Proyecto/Recordatorio.java
+++ src/Proyecto/Recordatorio.java
@@ -1,4 +1,10 @@
 package Proyecto;
 
+import java.time.LocalDate;
+
 public class Recordatorio {
+    private LocalDate hora; //hora de la toma
+    private int frecuenciaHoras; //cada cuanto repetir
+    private Medicamento medicamentoAsociado;
+
 }
