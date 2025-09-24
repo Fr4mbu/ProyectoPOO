8b0e265da4b97d185e59a43af90d7d9bfa143f09
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 17:23:43 -0300

    Implementar constructor de Insulina con herencia y parametros especificos

--- src/Proyecto/Insulina.java
+++ src/Proyecto/Insulina.java
@@ -5,4 +5,12 @@ public class Insulina extends Medicamento {
     private double nivelGlucosa;
     private int dosisRecomendada;
 
+    //constructor
+    private Insulina(String nombre, int dosis, int cantidad, String fecha, double nivelGlucosa, int dosisRecomendada) {
+        super(nombre, dosis, cantidad, fecha); // esto llama al constructor de Medicamento
+        this.nivelGlucosa = nivelGlucosa;
+        this.dosisRecomendada = dosisRecomendada;
+    }
+
+
 }
