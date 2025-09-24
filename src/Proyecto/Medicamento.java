07e304e1579a84107a65a571ba7f004852d3ced6
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 13:16:52 -0300

    Implementar atributos y constructor de clase Medicamento

--- src/Proyecto/Medicamento.java
+++ src/Proyecto/Medicamento.java
@@ -1,4 +1,18 @@
 package Proyecto;
 
+
 public class Medicamento {
+    private String nombre;
+    private int dosis; //para dosis en mg o unidades
+    private int cantidadDisponible;
+    private String fechaVencimiento;
+
+    //constructor
+    public Medicamento(String nombre, int dosis, int cantidad, String fecha) {
+        this.nombre = nombre;
+        this.dosis = dosis;
+        this.cantidadDisponible = cantidad;
+        this.fechaVencimiento = fecha;
+    }
+
 }
