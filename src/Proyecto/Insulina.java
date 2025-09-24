105dc70b0f2e77f876c75607b218e7b2ee5593f4
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 17:22:24 -0300

    Heredar clase Insulina de Medicamento y agregar atributos especificos

--- src/Proyecto/Insulina.java
+++ src/Proyecto/Insulina.java
@@ -1,4 +1,8 @@
 package Proyecto;
 
-public class Insulina {
+//heredada de medicamento
+public class Insulina extends Medicamento {
+    private double nivelGlucosa;
+    private int dosisRecomendada;
+
 }
