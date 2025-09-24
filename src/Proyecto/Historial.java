c409b60ef03759f3db2990e9483e540bf862e4f4
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 18:08:10 -0300

    Implementar constructor de Historial con inicializacion de ArrayList

--- src/Proyecto/Historial.java
+++ src/Proyecto/Historial.java
@@ -1,10 +1,15 @@
 package Proyecto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Historial {
     private List<String> registros; //lista de eventos de consumo
 
+    //constructor
+    public Historial() {
+        this.registros = new ArrayList<>();
+    }
 
 
 }
