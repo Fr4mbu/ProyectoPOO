3ac73847b0bb40d60636e8ca4ee775ab8201f172
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-25 20:14:03 -0300

    configurar ControladorPaciente con imports y atributos para MVC

---
+++ src/Proyecto/Controlador/ControladorPaciente.java
@@ -0,0 +1,17 @@
+package Proyecto.Controlador;
+
+import Proyecto.LoginPaciente;
+import Proyecto.Modelo.Paciente;
+import Proyecto.Modelo.Medicamento;
+import Proyecto.Modelo.Historial;
+import Proyecto.Paciente;
+import Proyecto.VentanaPaciente;
+import Proyecto.Vista.LoginPaciente;
+import Proyecto.Vista.VentanaPaciente;
+
+public class ControladorPaciente {
+    private Paciente paciente;
+    private LoginPaciente loginVista;
+    private VentanaPaciente ventanaVista;
+
+}
