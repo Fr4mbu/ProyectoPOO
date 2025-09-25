f38c6cc710010831ac9944c422879859044bc192
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-25 20:20:03 -0300

    implementar constructor y setter para configuracion MVC del controlador

--- src/Proyecto/Controlador/ControladorPaciente.java
+++ src/Proyecto/Controlador/ControladorPaciente.java
@@ -1,17 +1,24 @@
 package Proyecto.Controlador;
 
-import Proyecto.LoginPaciente;
-import Proyecto.Modelo.Paciente;
-import Proyecto.Modelo.Medicamento;
-import Proyecto.Modelo.Historial;
-import Proyecto.Paciente;
-import Proyecto.VentanaPaciente;
 import Proyecto.Vista.LoginPaciente;
+import Proyecto.Modelo.Paciente;
 import Proyecto.Vista.VentanaPaciente;
 
 public class ControladorPaciente {
     private Paciente paciente;
     private LoginPaciente loginVista;
     private VentanaPaciente ventanaVista;
+
+    //constructor con paciente y login
+    public ControladorPaciente(Paciente paciente, LoginPaciente loginVista) {
+        this.paciente = paciente;
+        this.loginVista = loginVista;
+    }
+
+    //asociar ventana principal luego del login
+    public void setVentanaPaciente(VentanaPaciente ventanaVista) {
+        this.ventanaVista = ventanaVista;
+    }
+
 
 }
