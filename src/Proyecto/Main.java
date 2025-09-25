5e0fbf5bddf07d5cb2ef0ddcdef9dc920e31821b
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-25 00:41:37 -0300

    Refactorizar Main: declarar Scanner y Paciente como variables estaticas

--- src/Proyecto/Main.java
+++ src/Proyecto/Main.java
@@ -1,24 +1,15 @@
 package Proyecto;
 
-//clase de prueba para ver si funciona la clase insulina
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        private static Scanner scanner = new Scanner(System.in);
+        private static Paciente paciente;
 
-        //crear insulina
-        Insulina insulina = new Insulina("Insulina", 10, 10, "2025 - 09 - 24", 200, 8);
 
-        //mostrar informacion y recomendacion
-        System.out.println("Nombre: " + insulina.getNombre());
-        System.out.println("Stock inicial: " + insulina.getCantidadDisponible());
-        insulina.recomendarDosis();
 
-        //tomar una dosis de insulina
-        insulina.tomarDosis();
-        System.out.println("Stock restante: " + insulina.getCantidadDisponible());
 
-        //cambiar nivel de glucosa y volver a recomendar
-        insulina.setNivelGlucosa(95);
-        insulina.recomendarDosis();
     }
 }
 
