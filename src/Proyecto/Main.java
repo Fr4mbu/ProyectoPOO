c0d19572070947298568081b87491f81d1c9d866
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-24 21:48:19 -0300

    Agrega prueba funcional de clase Medicamento en metodo main

--- src/Proyecto/Main.java
+++ src/Proyecto/Main.java
@@ -1,9 +1,23 @@
 package Proyecto;
 
-import java.util.Scanner;
-
+//clase de prueba para ver si funciona la clase medicamento
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+        //crear medicamento
+        Medicamento paracetamol = new Medicamento("Paracetamol", 500, 5, "2025-09-24");
+
+        //mostrar informacion inicial
+        System.out.println("Medicamento: " + paracetamol.getNombre());
+        System.out.println("Stock inicial: " + paracetamol.getCantidadDisponible());
+
+        //tomar dosis varias veces
+        paracetamol.tomarDosis(); //primera dosis
+        paracetamol.tomarDosis(); //sugunda
+        paracetamol.tomarDosis(); //tercera
+
+        //mostrar stock
+        System.out.println("Stock final: " + paracetamol.getCantidadDisponible());
     }
 }
+
