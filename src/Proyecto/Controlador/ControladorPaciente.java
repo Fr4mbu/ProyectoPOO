b33271e72259bfa771e8f1326aa5a78dc9eb7424
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-25 20:28:10 -0300

    implementar metodos del controlador para gestion de medicamentos

--- src/Proyecto/Controlador/ControladorPaciente.java
+++ src/Proyecto/Controlador/ControladorPaciente.java
@@ -1,5 +1,6 @@
 package Proyecto.Controlador;
 
+import Proyecto.Modelo.Medicamento;
 import Proyecto.Vista.LoginPaciente;
 import Proyecto.Modelo.Paciente;
 import Proyecto.Vista.VentanaPaciente;
@@ -19,6 +20,37 @@ public class ControladorPaciente {
     public void setVentanaPaciente(VentanaPaciente ventanaVista) {
         this.ventanaVista = ventanaVista;
     }
+
+    //registrar un medicamento
+    public void registrarMedicamento(Medicamento medicamento) {
+        paciente.agregarMedicamento(medicamento);
+        if(ventanaVista!=null){
+            ventanaVista.mostrarMensaje("Medicamento registrado: " + medicamento.getNombre());
+        }
+    }
+
+    //lista de medicamentos
+    public List<Medicamento> obtenerMedicamentos(){
+        return paciente.getListaMedicamentos();
+    }
+
+    //tomar m¿un medicamento por nombre
+    public void tomarMedicamento(String nombre){
+        boolean resultado = paciente.tomarMedicamento(nombre);
+        if(ventanaVista!=null){
+            if(resultado){
+                ventanaVista.mostrarMensaje("Se tomo correctamente: " + nombre);
+            } else {
+                ventanaVista.mostrarMensaje("No se encontro el medicamento: " + nombre);
+            }
+        }
+    }
+
+
+
+
+
+
 
 
 }
