f7e8ee9cfea10f95de2550ea39b4e57f84f30d7d
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-25 00:42:59 -0300

    Declarar Scanner y Paciente como variables estaticas de clase

--- src/Proyecto/Main.java
+++ src/Proyecto/Main.java
@@ -3,9 +3,8 @@ package Proyecto;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        private static Scanner scanner = new Scanner(System.in);
-        private static Paciente paciente;
+    private static Scanner scanner = new Scanner(System.in);
+    private static Paciente paciente;
 
 
 
