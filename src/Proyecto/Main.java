2c16381af432c12db7eaa48cb2ee4566cebc4bed
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-25 00:45:52 -0300

    Implementar flujo principal del sistema con menu y creacion de paciente

--- src/Proyecto/Main.java
+++ src/Proyecto/Main.java
@@ -6,9 +6,11 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static Paciente paciente;
 
+    public static void main(String[] args) {
+        System.out.println("----- Sistema de Gestion de Medicamentos -----");
 
-
-
+        crearPaciente(); //se crea al paciente
+        ejecutarMenu();
     }
 }
 
