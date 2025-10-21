c1716c4f9d0a1171987a5bd27dff9b955b0bba83
Fr4mbu <f.aguayo03@ufromail.cl>
2025-10-20 21:12:13 -0300

    Definir estructura base de GestorDatosPaciente con rut y archivos

--- src/proyecto/modelo/GestorDatosPaciente.java
+++ src/proyecto/modelo/GestorDatosPaciente.java
@@ -2,4 +2,13 @@ package proyecto.modelo;
 
 //clase responsable de la persistencia de datos de un paciente especifico (los carga y los guarda)
 public class GestorDatosPaciente {
+    private final Paciente paciente;
+    private final String rutPaciente;
+
+    private final String ARCHIVO_MEDICAMENTOS;
+    private final String ARCHIVO_RECORDATORIO;
+    private final String ARCHIVO_HISTORIAL;
+
+
+
 }
