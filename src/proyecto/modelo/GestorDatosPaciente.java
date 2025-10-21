f0f5e227e3f1017e5554925a4a13e7874738eec1
Fr4mbu <f.aguayo03@ufromail.cl>
2025-10-20 21:17:13 -0300

    Implementar constructor de GestorDatosPaciente con validacion y nombres de archivos

--- src/proyecto/modelo/GestorDatosPaciente.java
+++ src/proyecto/modelo/GestorDatosPaciente.java
@@ -9,6 +9,19 @@ public class GestorDatosPaciente {
     private final String ARCHIVO_RECORDATORIO;
     private final String ARCHIVO_HISTORIAL;
 
+    public GestorDatosPaciente(Paciente paciente) {
+        if (paciente == null) {
+            throw new IllegalArgumentException("El paciente no puede ser nulo");
+        }
+        this.paciente = paciente;
+        this.rutPaciente = paciente.getRut().replace("[^a-zA-Z0-9.-]", "_");
+
+        //definir los nombres de los archivos especificos del paciente
+        this.ARCHIVO_MEDICAMENTOS = rutPaciente + "_medicamentos.csv";
+        this.ARCHIVO_RECORDATORIO = rutPaciente + "_recordorios.csv";
+        this.ARCHIVO_HISTORIAL = rutPaciente + "_historial.csv";
+    }
+
 
 
 }
